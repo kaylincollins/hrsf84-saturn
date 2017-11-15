a3f136795712d0b1dd30144ae0d6239bb8d1cbc1
Dustin Huang <mail@dustinhuang.com>
2017-11-15 10:52:45 -0800

    Changed hello world to picvoyage

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.get('/', (req, res) => res.send('PicVoyage'));
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`listening on ${port}`));
