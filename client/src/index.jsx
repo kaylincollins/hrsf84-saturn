995d6388bed6296fb3cf1befb019d6be7542bc03
Katherine Fickel <katherine.fickel@gmail.com>
2017-11-15 16:25:10 -0800

    Rough outline of homepage pictures and search bar

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,4 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <img src="/images/logo.png" />
+      <HomePage/>
+    );
+  }
+}
+
 ReactDOM.render(<App />, document.getElementById('app'));
+
