342a533274f445cb6ecbd81e107f9bad0eb4e0fc
Dustin Huang <mail@dustinhuang.com>
2017-11-15 16:11:10 -0800

    Add initial react rendering

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static(__dirname + '/../client/public'));
+
 app.get('/', (req, res) => res.send('PicVoyage best app ever'));
 
 const port = process.env.PORT || 3000
