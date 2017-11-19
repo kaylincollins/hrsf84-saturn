a996983012c9222c60f568e2d6f9f6f5325533d6
Dustin Huang <mail@dustinhuang.com>
2017-11-19 12:31:25 -0800

    Add catchall route for client side routing

--- server/index.js
+++ server/index.js
@@ -6,6 +6,10 @@ app.use(express.static(`${__dirname}/../client/public`));
 
 app.get('/', (req, res) => res.send('PicVoyage best app ever'));
 
+app.get('*', (req, res) => {
+  res.sendFile('index.html', { root: `${__dirname}/../client/public/` });
+});
+
 const port = process.env.PORT || 3000;
 const server = app.listen(port);
 
