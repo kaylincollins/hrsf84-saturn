7924892217222cf92c91a21b1f99042b6b5a025f
Katherine Fickel <katherine.fickel@gmail.com>
2017-11-20 10:20:58 -0800

    Update homepage search bar and pictures

---
+++ config.js
@@ -0,0 +1,8 @@
+var tokenInfo = 'Hl6YHkmmLcnRE7p-QyBfzrTCzhaH58iJSyxrpA33cbf2bHc_aOGkQD3-Pe4m1_nSQyfzo8GJsnBtut6QQXMND1cbop62hCmWXh1xgry95rP4pgUaqetaela1D28TWnYx'
+// {
+//   grant_type: 'client_credentials',
+//   client_id: 'fZaZSNBqpGMwy6rEPYTnZA',
+//   client_secret: '1nzFrIeTEVHCtBMktGAVCzbdjCKSstPIUqTn1nE9fzJNLyzlAfiRjZLINS6XLU90',
+// }
+
+module.exports = tokenInfo;
