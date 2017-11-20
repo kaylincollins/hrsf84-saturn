4da0f31bbecfce9212682881ab931772df119c68
Kaylin <kaylin.collins68@gmail.com>
2017-11-20 15:07:49 -0800

    added body parser to server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 app.use(express.static(`${__dirname}/../client/public`));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 app.get('/', (req, res) => res.send('PicVoyage best app ever'));
 
