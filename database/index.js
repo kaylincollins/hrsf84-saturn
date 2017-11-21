7f91f93a5f354e1404307cd36e1bfaba00ff95d8
Katherine Fickel <katherine.fickel@gmail.com>
2017-11-21 10:20:58 -0800

    Uncommented out database

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
-// const mongoose = require("mongoose");
-// const mongoUri = `mongodb://${process.env.SATURNDB}`;
+const mongoose = require("mongoose");
+const mongoUri = `mongodb://${process.env.SATURNDB}`;
 
-// const db = mongoose.connect(mongoUri);
+const db = mongoose.connect(mongoUri);
 
-// module.exports = db;
+module.exports = db;
